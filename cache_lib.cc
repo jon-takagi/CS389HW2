5f3f2ca2e15f3bb35f94e8ae5d65a64f21ed91cf
jon-takagi <jontakagi@reed.edu>
2020-02-25 00:38:05 +0000

    create skeleton of cache_lib

---
+++ cache_lib.cc
@@ -0,0 +1,20 @@
+#include <cache.hh>
+
+Cache::Cache(index_type maxmem, evictor_type evictor = [](){ return 0; }, hash_func hasher = std::hash<std::string>()){
+return null;
+}
+
+void Cache::set(key_type key, val_type val, index_type size) {
+
+}
+
+Cache::val_type Cache::get(key_type key, index_type& val_size) const {
+
+}
+
+void Cache::del(key_type key) {
+
+}
+Cache::index_type Cache::space_used() const{
+
+}
