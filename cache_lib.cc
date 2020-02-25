acf4b7583d2950bc0baf8b66f66c79bb23ad5cf6
jon-takagi <jontakagi@reed.edu>
2020-02-25 00:40:34 +0000

    update sample code

--- cache_lib.cc
+++ cache_lib.cc
@@ -9,12 +9,12 @@ void Cache::set(key_type key, val_type val, index_type size) {
 }
 
 Cache::val_type Cache::get(key_type key, index_type& val_size) const {
-
+return null;
 }
 
 void Cache::del(key_type key) {
 
 }
 Cache::index_type Cache::space_used() const{
-
+return null;
 }
