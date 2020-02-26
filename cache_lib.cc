ce320b9d42f2ba3a56140ebe7c4993e105d91a09
jon-takagi <jontakagi@reed.edu>
2020-02-26 20:24:16 +0000

    whyyyyyyyy

--- cache_lib.cc
+++ cache_lib.cc
@@ -2,66 +2,77 @@
 #include <tuple>
 #include "cache.hh"
 
-using entry_type = std::pair<val_type, size_type> //To better track size for entries
 
-Cache::Impl{
+using entry_type = std::pair<Cache::val_type, Cache::size_type> //To better track size for entries
+
+class Cache::Impl{
     public:
-        std::unordered_map<Cache::key_type, Cache::val_type, hash_func> dict;//might be an error here; might be able to use default hash type
-        size_type size;
+        std::unordered_map<key_type, Cache::val_type, hash_func> dict_;//might be an error here; might be able to use default hash type
+        size_type size_;
+        size_type maxmem_;
+        Evictor* evictor_;
 };
 
-Cache::Cache(index_type maxmem, evictor_type evictor = [](){ return 0; }, hash_func hasher = std::hash<std::string>()){
-    std::unordered_map<Cache::key_type, Cache::val_type, hash_func> dict(4, hasher);//set to 4 buckets at start
-    pImpl->dict = dict;//hope this works..
-    pImpl->dict.max_load_factor(0.75);
-    pImpl->size = 0;
+Cache::Cache(size_type maxmem,float max_load_factor, Evictor* evictor, hash_func hasher)
+ : pImpl_(new Impl()) {
+    std::unordered_map<key_type, Cache::val_type, hash_func> dict(4,hasher);//set to 4 buckets at start
+    pImpl_->dict_ = dict;//hope this works..
+    pImpl_->dict_.max_load_factor(max_load_factor);
+    pImpl_->size_ = 0;
+    pImpl_->maxmem_ = maxmem;
+    pImpl_->evictor_ = evictor;
 }
 
+Cache::~Cache() {
+
+}
 //Basic version: Allows setting of key only when there's enough size, otherwise rejects
-//Then copies by iterating over val to make new entry to put into pImpl dict
-void Cache::set(key_type key, val_type val, index_type size) {
-    if(pImpl->size + size <= maxmem){
-        val_type entry_val[size];
-        int i = 0;
-        while(val[i] != '\0'){ //Searching for null terminator
-            entry_val[i] = val[i];
-        }
-        pImpl->dict.insert({key,entry_val});
-        pImpl->size += size;
-    }
-}
+//Then copies by iterating over val to make new entry to put into pImpl_ dict
+// void Cache::set(key_type key, val_type val, size_type size) {
+    // if(pImpl_->size_ + size <= pImpl_->maxmem_){
+    //     val_type entry_val[size];
+    //     int i = 0;
+    //     while(val[i] != '\0'){ //Searching for null terminator
+    //         entry_val[i] = val[i];
+    //     }
+    //     std::pair<Cache::val_type, Cache::size_type> entry = std::make_pair(key,entry_val);
+    //     pImpl_->dict_.insert(entry);
+    //     pImpl_->size_ += size;
+    // }
+// }
 
-Cache::val_type Cache::get(key_type key, index_type& val_size) const {
+Cache::val_type Cache::get(key_type key, size_type& val_size) const {
     size_type size = 0;
     //Iterate through or just grab the size?
     val_size = size;
-    return pImpl->dict[key];
+    return pImpl_->dict_[key];
 }
 
-void Cache::del(key_type key) {
-    val_type val=pImpl->dict[key];
+bool Cache::del(key_type key) {
+    val_type val=pImpl_->dict_[key];
     int size = 0;
     while(val[size] != '\0'){ //Searching for null terminator
         size += 1;
     }
-    pImpl->size -= size;
-    pImpl->dict.erase(key);
+    pImpl_->size_ -= size;
+    pImpl_->dict_.erase(key);
+    return true;
 }
 
 Cache::size_type Cache::space_used() const{
     /*
     Cache::size_type size = 0;
-    for (std::pair<Cache::key_type, Cache::val_type> element : pImpl->dict)
+    for (std::pair<Cache::key_type, Cache::val_type> element : pImpl_->dict_)
     {
     	size += sizeof(element.second);
     }
     return size;
     */
     //faster
-    return pImpl->size;
+    return pImpl_->size_;
 }
 
 void Cache::reset() {
-    pImpl->dict.erase(pImpl->dict.begin(), pImpl->dict.end());
-    pImpl->size = 0;
+    pImpl_->dict_.erase(pImpl_->dict_.begin(), pImpl_->dict_.end());
+    pImpl_->size_ = 0;
 }
