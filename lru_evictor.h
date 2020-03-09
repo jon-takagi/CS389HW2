eae12c46a0af31ceb42f2dcf19565086b91ba689
Eli Poppele <ehpoppele@gmail.com>
2020-03-09 01:08:48 +0000

    LRU evictor done (?)

--- lru_evictor.h
+++ lru_evictor.h
@@ -1,11 +1,27 @@
 #include "evictor.hh"
-#include <queue>        // std::queue
-#include <list>         // std::list
+#include <unordered_map> // std::unordered_map
+
+class LLNode {
+        int age_;
+        LLNode* front_;
+        LLNode* rear_;
+    LLNode(int age) {
+        age_ = age;
+    }
+    LLNode(int age, LLNode* front, LLNode* rear) {
+        age_ = age;
+        front_ = front;
+        rear_ = rear;
+    }
+
+};
 
 // using node_type = whatever the type of the nodes of the list are
 class LruEvictor:public Evictor {
     private:
-        std::list<key_type> contents_;
+        std::unordered_map<const key_type&, *LLNode> map_;
+        LLNode* head_;
+        LLNode* tail_;
     public:
         void touch_key(const key_type&);
         const key_type evict();
